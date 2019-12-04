e257a057a087b0da8b6d5aa6beadd4698d1ed2e3
Trippwire2000 <58035903+Trippwire2000@users.noreply.github.com>
2019-12-04 23:10:48 +1000

    Add files via upload
    Login working, set window geometry, neatened layout.
     need to fix dice images, betting function

--- Dice.py
+++ Dice.py
@@ -1,92 +1,132 @@
-import tkinter
+from tkinter import *
+from PIL import ImageTk, Image
 from random import randint
 import pickle
 
-#from PIL import ImageTk,Image
+users = pickle.load(open("users.p", "rb"))
+# users = {'d': 290}
+# username = 'd'
 
-predict = 0
-betok = False
-bet = 0
-
-users = {'d': 200}
-username = 'd'
-
-window = tkinter.Tk()
+# init window
+window = Tk()
 window.title('Dice')
-
-balancedisplaytitle = tkinter.Label(window, text='Credits - ')
-balancedisplay = tkinter.Entry(window)
-balance = 100
-
-betmsgbox=tkinter.Entry(window, width=50)
+window.geometry("640x320")
+window.resizable(0, 0)
 
 
-dice1label = tkinter.Label(window, text='First Die')
-dice2label = tkinter.Label(window, text='Second Die')
-dicetotallabel = tkinter.Label(window, text='Total')
-dice1 = tkinter.Entry(window, width=1)
-dice2 = tkinter.Entry(window, width=1)
-dicetotal = tkinter.Entry(window, width=2)
+betmsgbox = Entry(window, width=50)
+verify = Entry(window, width=42)
+
+choosenum = Label(window, text='   Choose your number')
+choosehilo = Label(window, text='               Will the rolled total be Higher or Lower than your chosen number?')
+rollbutton = Button(window, text='Set the bet', command=lambda: [clear2, setbet(predict)])
+
+# init buttons
+
+button2 = Button(window, text='2', command=lambda: setpredict(2))
+button3 = Button(window, text='3', command=lambda: setpredict(3))
+button4 = Button(window, text='4', command=lambda: setpredict(4))
+button5 = Button(window, text='5', command=lambda: setpredict(5))
+button6 = Button(window, text='6', command=lambda: setpredict(6))
+button7 = Button(window, text='7', command=lambda: setpredict(7))
+button8 = Button(window, text='8', command=lambda: setpredict(8))
+button9 = Button(window, text='9', command=lambda: setpredict(9))
+button10 = Button(window, text='10', command=lambda: setpredict(10))
+button11 = Button(window, text='11', command=lambda: setpredict(11))
+button12 = Button(window, text='12', command=lambda: setpredict(12))
+
+hibutton = Button(window, text='Higher', command=lambda: [clear1(), sethilo('higher')])
+lobutton = Button(window, text='Lower', command=lambda: [clear1(), sethilo('lower')])
+precisebutton = Button(window, text='Precise', command=lambda: [clear1(), sethilo('precisely')])
+# buttonquit = Button(window, text='Save and exit', command=window.quit)
 
 
-verify = tkinter.Entry(window, width=42)
+def login():
+    global username
+    window.grid_columnconfigure((0, 1, 2), weight=1)
+    window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+    usernamelabel=Label(window,text="What is your username? ")
+    usernameinput=Entry(window, width=22)
+    loginbutton=Button(window, text='Login', command=lambda: getusername())
+    usernamelabel.grid(row=1, column=1)
+    usernameinput.grid(row=2, column=1)
+    loginbutton.grid(row=3, column=1)
 
-choosenum = tkinter.Label(window, text='Choose your number')
-choosehilo = tkinter.Label(window, text='Will the rolled total be Higher or Lower than your chosen number?')
-rollbutton = tkinter.Button(window, text='ROLL THE DICE', command=lambda: rolling())
+    def getusername():
+        global username
+        username = usernameinput.get()
 
-#buttonquit = Button(window, text='Exit program', command= root.quit)
+        if username in users:
+            welcomemessage = Label(text='Welcome back ' + username + '\n' + 'Remaining balance is ' + str(users[username]))
+            balance = (users[username])
+        if username not in users:
+            welcomemessage = Label(text='Hi ' + username + ', You start with 100 credit')
+            users[username] = 100
 
+        # Clears the screen
+        usernamelabel.grid_forget()
+        usernameinput.grid_forget()
+        loginbutton.grid_forget()
 
-button2 = tkinter.Button(window, text='2', command=lambda: setpredict(2))
-button3 = tkinter.Button(window, text='3', command=lambda: setpredict(3))
-button4 = tkinter.Button(window, text='4', command=lambda: setpredict(4))
-button5 = tkinter.Button(window, text='5', command=lambda: setpredict(5))
-button6 = tkinter.Button(window, text='6', command=lambda: setpredict(6))
-button7 = tkinter.Button(window, text='7', command=lambda: setpredict(7))
-button8 = tkinter.Button(window, text='8', command=lambda: setpredict(8))
-button9 = tkinter.Button(window, text='9', command=lambda: setpredict(9))
-button10 = tkinter.Button(window, text='10', command=lambda: setpredict(10))
-button11 = tkinter.Button(window, text='11', command=lambda: setpredict(11))
-button12 = tkinter.Button(window, text='12', command=lambda: setpredict(12))
-
-hibutton = tkinter.Button(window, text='Higher', command=lambda: sethilo('higher'))
-lobutton = tkinter.Button(window, text='Lower', command=lambda: sethilo('lower'))
-precisebutton = tkinter.Button(window, text='Precise', command=lambda: sethilo('precisely'))
+        welcomemessage.grid(row=1, column=1)
+        startbutton=Button(text='Start', command=lambda: [startbutton.grid_forget(), welcomemessage.grid_forget(), start()])
+        startbutton.grid(row=2, column=1)
 
 
-balancedisplaytitle.grid(row=0, column=8)
-balancedisplay.delete(0, 99999999)
-balancedisplay.insert(0, str(balance))
-balancedisplay.grid(row=0, column=9, columnspan=2)
+def start():
+    # reset grid config
+    window.grid_columnconfigure((0, 1, 2), weight=0)
+    window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=0)
 
-choosenum.grid(row=1, columnspan=9, column=1)
-button2.grid(row=2, column=0, padx=10, pady=10)
-button3.grid(row=2, column=1, padx=10, pady=10)
-button4.grid(row=2, column=2, padx=10, pady=10)
-button5.grid(row=2, column=3, padx=10, pady=10)
-button6.grid(row=2, column=4, padx=10, pady=10)
-button7.grid(row=2, column=5, padx=10, pady=10)
-button8.grid(row=2, column=6, padx=10, pady=10)
-button9.grid(row=2, column=7, padx=10, pady=10)
-button10.grid(row=2, column=8, padx=10, pady=10)
-button11.grid(row=2, column=9, padx=10, pady=10)
-button12.grid(row=2, column=10, padx=10, pady=10)
+    balancedisplay = Label(window, text='Credits - ' + str(users[username]))  # Balance is in dict users
+
+    # Placing widgets onscreen
+    balancedisplay.grid(row=0, column=0, columnspan=5)
+    choosenum.grid(row=1, columnspan=9, column=1)
+    button2.grid(row=2, column=0, padx=10, pady=10)
+    button3.grid(row=2, column=1, padx=10, pady=10)
+    button4.grid(row=2, column=2, padx=10, pady=10)
+    button5.grid(row=2, column=3, padx=10, pady=10)
+    button6.grid(row=2, column=4, padx=10, pady=10)
+    button7.grid(row=2, column=5, padx=10, pady=10)
+    button8.grid(row=2, column=6, padx=10, pady=10)
+    button9.grid(row=2, column=7, padx=10, pady=10)
+    button10.grid(row=2, column=8, padx=10, pady=10)
+    button11.grid(row=2, column=9, padx=10, pady=10)
+    button12.grid(row=2, column=10, padx=10, pady=10)
 
 
 def setpredict(num):
     global predict
-    predict = (num)
-    showhilo()
+    predict = num
 
-def showhilo():
+    # place buttons
     choosehilo.grid(row=4, columnspan=10, column=0)
-    hibutton.grid(row=5, column=1, columnspan=4, padx=20, pady=50)
+    hibutton.grid(row=5, column=0, columnspan=4, padx=20, pady=50)
+    precisebutton.grid(row=5, column=4, columnspan=4, padx=20, pady=1)
     lobutton.grid(row=5, column=8, columnspan=4, padx=20, pady=50)
-    precisebutton.grid(row=5, column=5, columnspan=4, padx=20, pady=50)
 
 
-#higher or lower input
+def clear1():
+    button2.grid_remove()
+    button3.grid_remove()
+    button4.grid_remove()
+    button5.grid_remove()
+    button6.grid_remove()
+    button7.grid_remove()
+    button8.grid_remove()
+    button9.grid_remove()
+    button10.grid_remove()
+    button11.grid_remove()
+    button12.grid_remove()
+    choosenum.grid_remove()
+    choosehilo.grid_remove()
+    hibutton.grid_remove()
+    lobutton.grid_remove()
+    precisebutton.grid_remove()
+
+
+# higher or lower input
 def sethilo(hilopr):
     hi = lo = pr = False
     if hilopr == 'higher':
@@ -101,59 +141,84 @@ def sethilo(hilopr):
     elif hilopr == 'precisely':
         verify.insert(0, 'You have chosen that the roll will be exactly ' + str(predict))
 
-#show verification and roll button
-    verify.grid(row=6, column=0, columnspan=9)
+    verify.grid(row=6, column=0, columnspan=6)
     rollbutton.grid(row=6, column=9, columnspan=3)
 
 
-def rolling():
-    betok = False
-    while betok == False:
-        betmsgbox.grid(row=7, column=2)
+def setbet(prediction):    # fix me
+        betok = False
+        while betok != True:
+            whatbet = Label(text='What is your bet?')
+            whatbet.grid(row=2, column=3)
+            betentry = Entry(width=5)
+            betentry.grid(row=2, column=4)
+            go = Button(text='OK', command=lambda: bbet())
+            go.grid(row=2, column=5)
 
-        try:
-            betmsgbox.delete(0, 999)
-            betmsgbox.insert(0, 'What is your bet?')
+            def bbet():
+                bet = betentry.get()
+
 
             if bet <= (users[username]):
                 betok = True
             else:
                 betmsgbox.delete(0, 999)
                 betmsgbox.insert(0, 'Your balance is too low, max bet is ' + str(users[username]) + '.')
-        except:
-            betmsgbox.delete(0, 999)
-            betmsgbox.insert(0, 'Enter a number only, no decimal.')
 
 
-#generate random dice roll
+
+def diceroll():
+    # generate random dice roll
     d1 = randint(1, 6)
     d2 = randint(1, 6)
-    tot = int(d1 + d2)
-
-#put the elements on screen
-    dice1label.grid(row=8, column=0)
-    dice1.grid(row=8, column=2)
-    dice2label.grid(row=8, column=4)
-    dice2.grid(row=8, column=5)
-    dicetotallabel.grid(row=8, column=7)
-    dicetotal.grid(row=8, column=8)
-
-#clear the elements and insert new values
-    dice1.delete(0, 2)
-    dice2.delete(0, 2)
-    dicetotal.delete(0, 3)
-    dice1.insert(0, d1)
-    dice2.insert(0, d2)
-    dicetotal.insert(0, tot)
+    tot = d1 + d2
 
 
+    # init dice images
+
+    if d1 == 1:
+        dice1 = ImageTk.PhotoImage(Image.open('1.png'))
+    elif d1 == 2:
+        dice1 = ImageTk.PhotoImage(Image.open('2.png'))
+    elif d1 == 3:
+        dice1 = ImageTk.PhotoImage(Image.open('3.png'))
+    elif d1 == 4:
+        dice1 = ImageTk.PhotoImage(Image.open('4.png'))
+    elif d1 == 5:
+        dice1 = ImageTk.PhotoImage(Image.open('5.png'))
+    else:
+        dice1 = ImageTk.PhotoImage(Image.open('6.png'))
+
+    diceshow1 = Label(image=dice1, padx=5, pady=5)
+
+    if d2 == 1:
+        dice2 = ImageTk.PhotoImage(Image.open('1.png'))
+    elif d2 == 2:
+        dice2 = ImageTk.PhotoImage(Image.open('2.png'))
+    elif d2 == 3:
+        dice2 = ImageTk.PhotoImage(Image.open('3.png'))
+    elif d2 == 4:
+        dice2 = ImageTk.PhotoImage(Image.open('4.png'))
+    elif d2 == 5:
+        dice2 = ImageTk.PhotoImage(Image.open('5.png'))
+    else:
+        dice2 = ImageTk.PhotoImage(Image.open('6.png'))
+
+    diceshow2 = Label(image=dice2, padx=5, pady=5)
+
+    clear2()
+
+    diceshow1.grid(row=0, column=0)
+    diceshow2.grid(row=0, column=1)
+    dicetotal=Label(width=10, text='Total - ' + str(tot))
+    dicetotal.grid(row=1, column=0)
+
+
+def clear2():
+    verify.grid_remove()
+    rollbutton.grid_remove()
+
+login()
+
+# pickle.dump(users, open("users.p", "wb"))
 window.mainloop()
-"""
-#dropdown menu for betting
-var = StringVar(window)
-var.set(str(1)) # initial value
-
-option = OptionMenu(window, var, str(1), str(2), str(5), str(10), str(20), str(50), str(100))
-option.pack()
-
-"""
